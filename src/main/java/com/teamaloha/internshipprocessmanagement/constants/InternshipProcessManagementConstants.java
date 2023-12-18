86b4fd37cb7241a81e436cdbf96fbd3b906eecce
Ahmet Yigit <82308205+Mens1s@users.noreply.github.com>
2023-12-18 12:27:56 +0300

    50 minutes per login

--- src/main/java/com/teamaloha/internshipprocessmanagement/constants/InternshipProcessManagementConstants.java
+++ src/main/java/com/teamaloha/internshipprocessmanagement/constants/InternshipProcessManagementConstants.java
@@ -1,5 +1,5 @@
 package com.teamaloha.internshipprocessmanagement.constants;
 
 public class InternshipProcessManagementConstants {
-    public static final int USER_EXPIRE_TIME_IN_SECONDS = 5 * 60;
+    public static final int USER_EXPIRE_TIME_IN_SECONDS = 50 * 60;
 }
